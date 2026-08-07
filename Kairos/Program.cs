3ae06b1283ae2aefa4a9dbb361e11c313b9ed944
jiwon0612 <iololloli9976@gmail.com>
2026-08-07 20:18:47 +0900

    fix: Magirate

--- Kairos/Program.cs
+++ Kairos/Program.cs
@@ -115,6 +115,12 @@ namespace Kairos
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<KairosDbContext>();
+                dbContext.Database.Migrate();
+            }
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
