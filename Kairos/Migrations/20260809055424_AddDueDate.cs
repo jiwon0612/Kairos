b67cd99c2776f5f1003f2b1c2406872837b7dd2f
jiwon0612 <iololloli9976@gmail.com>
2026-08-09 14:55:08 +0900

    fix: Migration

---
+++ Kairos/Migrations/20260809055424_AddDueDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kairos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDueDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Todos",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasDueTime",
+                table: "Todos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "HasDueTime",
+                table: "Todos");
+        }
+    }
+}
